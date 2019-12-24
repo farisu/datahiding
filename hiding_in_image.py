e75df57b86d3f5b592ac3cb375081de1e3ebe420
farisu <faris.uhlig@outlook.de>
2019-12-24 16:01:12 +0100

    Added testfiles

--- hiding_in_image.py
+++ hiding_in_image.py
@@ -1,2 +1,10 @@
 from PIL import Image
 
+imgage_for_hiding = Image.open('image.png')
+pixelmap = imgage_for_hiding.load()
+file_to_hide = open("test.txt",'r')
+print((pixelmap[1])
+
+
+
+imgage_for_hiding.save('test.png')
